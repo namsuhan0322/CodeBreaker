ea32d656a47ca7a25f84e293717a57fee97dabac
namsuhan0322 <rnatpakfl@gmail.com>
2025-11-10 20:25:54 +0900

    [Feat] .prettierrc 추가
    편의성인데 구글에 prettier 검색해서 적용하실분은 하시면 됩니다.

--- Node/GameServer.js
+++ Node/GameServer.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const mysql = require("mysql2/promise");
+const express = require('express');
+const mysql = require('mysql2/promise');
 const app = express();
 
-require("dotenv").config();
+require('dotenv').config();
 const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWARD;
@@ -12,26 +12,26 @@ const port = process.env.PORT;
 app.use(express.json());
 
 const pool = mysql.createPool({
-  host,
-  user,
-  password,
-  database,
+    host,
+    user,
+    password,
+    database,
 });
 
 // 테스트 게임 단어 조회
-app.get("/gameword/:wordId", async (req, res) => {
-  try {
-    const [gameword] = await pool.query(
-      "SELECT id, room_id, round_number, scrambled_word, correct_word FROM game_words WHERE id = ?",
-      [req.params.wordId]
-    );
-    console.log(gameword);
-    res.json(gameword);
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
+app.get('/gameword/:wordId', async (req, res) => {
+    try {
+        const [gameword] = await pool.query(
+            'SELECT id, room_id, round_number, scrambled_word, correct_word FROM game_words WHERE id = ?',
+            [req.params.wordId],
+        );
+        console.log(gameword);
+        res.json(gameword);
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 });
 
 app.listen(port || 4000, () => {
-  console.log(`서버 실행 중 : ${port}`);
+    console.log(`서버 실행 중 : ${port}`);
 });
