1db13e05c5718c1929c7d163507ab570a88d52c3
fti11 <mxonstruck@gmail.com>
2025-12-15 09:53:20 +0900

    [Fix] 수정

--- Node/GameServer.js
+++ Node/GameServer.js
@@ -5,7 +5,7 @@ const app = express();
 require('dotenv').config();
 const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
-const password = process.env.DB_PASSWARD;
+const password = process.env.DB_PASSWORD;
 const database = process.env.DB_DATABASE;
 const port = process.env.PORT;
 
